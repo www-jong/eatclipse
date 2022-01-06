1002c835495bebb87da3be31b1c195af152902cf
Chanhyun Wang <rubberramen@naver.com>
2022-01-06 16:32:16 +0900

    아놔 돌겠다!!!!!!!!!

---
+++ src/main/java/com/example/eatclipse/controller/member/CustController.java
@@ -0,0 +1,7 @@
+package com.example.eatclipse.controller.member;
+
+public class CustController {
+	
+	// 다시 테스트 아놔 다 날라갔네 증말...
+
+}
--- src/main/java/com/example/eatclipse/controller/member/custController.java
+++
@@ -1,5 +0,0 @@
-package com.example.eatclipse.controller.member;
-
-public class custController {
-
-}
